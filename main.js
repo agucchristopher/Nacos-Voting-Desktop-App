0b4644d5a55329af61678bb702c05b3718dcbc3d
Agu C. Christopher <aguchris740@gmail.com>
2023-06-05 13:17:12 -0700

    Added Raqeeb files

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Notification } = require("electron");
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
   });
 
+  const NOTIFICATION_TITLE = "Basic Notification";
+  const NOTIFICATION_BODY = "Notification from the Main process";
+
+  new Notification({
+    title: NOTIFICATION_TITLE,
+    body: NOTIFICATION_BODY,
+    sound: "./assets/beep.mp3",
+  }).show();
   win.loadFile("./pages/index.html");
 };
 
